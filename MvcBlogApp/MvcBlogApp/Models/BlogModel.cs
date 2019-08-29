fee55bbb3e8203ee25131c2055b29fbc1d551278
Ali Bulut <alibulut@yahoo.com>
2019-08-29 14:14:46 +0300

    blog list was created on homepage

---
+++ MvcBlogApp/MvcBlogApp/Models/BlogModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBlogApp.Models
+{
+    public class BlogModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Explanation { get; set; }
+        public string Image { get; set; }
+        public DateTime UploadDate { get; set; }
+        public bool Confirmation { get; set; }
+        public bool IsHomepage { get; set; }
+    }
+}
