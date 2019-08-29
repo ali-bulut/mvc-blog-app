661dbf2182e4376108f95fa60f9fd2a5dc453c8f
Ali Bulut <alibulut@yahoo.com>
2019-08-29 11:32:33 +0300

    entity classes were created

---
+++ MvcBlogApp/MvcBlogApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcBlogApp.MvcApplication" Language="C#" %>
---
+++ MvcBlogApp/MvcBlogApp/Models/Blog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBlogApp.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Explanation { get; set; }
+        public string Image { get; set; }
+        public string Content { get; set; }
+        public DateTime UploadDate { get; set; }
+        public bool Confirmation { get; set; }
+        public bool IsHomepage { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
